fc25c6d107609ef76cb5be034343acb0507b44ce
Prachi Shinde <prachimshinde23@gmail.com>
2026-03-04 20:17:10 +0530

    Set secure cookie options for production (SameSite=None, Secure)

--- Backend/Backend/controllers/userController.js
+++ Backend/Backend/controllers/userController.js
@@ -67,9 +67,8 @@ const loginController = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      // sameSite: "strict",
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 24 * 60 * 60 * 1000
     })
     return res.status(200).send({
